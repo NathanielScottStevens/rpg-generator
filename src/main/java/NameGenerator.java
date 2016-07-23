f2e350273a474bf7166b847bb0370e6084a55bf2
NathanielScottStevens <Nathaniel.Scott.Stevens@gmail.com>
2016-07-23 17:18:59 -0400

    NameGenerator working. Removed .gradle files from repo

--- src/main/java/NameGenerator.java
+++ src/main/java/NameGenerator.java
@@ -1,16 +1,57 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class NameGenerator {
 
-    public static String getFirstName(Sex sex){
-        return null;
+    static ArrayList<String> maleFirstNames = new ArrayList<String>();
+    static ArrayList<String> femaleFirstNames = new ArrayList<String>();
+    static ArrayList<String> lastNames = new ArrayList<String>();
+
+    static {
+        initArray("male-first-names.txt", maleFirstNames);
+        initArray("female-first-names.txt", femaleFirstNames);
+        initArray("last-names.txt", lastNames);
     }
 
-    public static String getLastName(){
-        return null;
+    static void initArray(String fileName, ArrayList<String> array){
+        Path path = Paths.get("build/resources/main/" + fileName);
+
+        try {
+            Scanner scanner =  new Scanner(path);
+            while (scanner.hasNextLine()){
+                array.add(scanner.nextLine());
+            }
+        }catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    static int getRandom(int min, int max){
+        return ThreadLocalRandom.current().nextInt(min, max);
+    }
+
+    public static String getFirstName(Sex sex){
+        switch (sex){
+            default:
+            case Male:
+                return maleFirstNames.get(getRandom(0, maleFirstNames.size()));
+            case Female:
+                return femaleFirstNames.get(getRandom(0, femaleFirstNames.size()));
+        }
+    }
+
+    public static String getLastName() {
+        return lastNames.get(getRandom(0, lastNames.size()));
     }
 
     public static String getFullName(Sex sex){
-        return null;
+        String first = getFirstName(sex);
+        String last = getLastName();
+        return first + " " + last;
     }
 
     public static void main(String[] args){
